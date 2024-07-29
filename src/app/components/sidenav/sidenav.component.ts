0b37c316d40eb8ec98601401b1268174821cabd1
ranjani <ranjaniknair@gmail.com>
2024-07-29 15:52:57 +0530

    Added Admin to sidepanel

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -5,6 +5,7 @@ import { RouterLink } from '@angular/router';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule} from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
@@ -14,9 +15,17 @@ import { CommonModule} from '@angular/common';
   styleUrl: './sidenav.component.css'
 })
 export class SidenavComponent  {
-  isExpanded = true;
 
-  toggleSidebar() {
+  constructor(private router: Router) {}
+  isExpanded = true; // Default state of sidenav
+  isAdmin = false; // Toggle this based on app's logic
+
+  toggleSidebar(): void {
     this.isExpanded = !this.isExpanded;
+  }
+
+  adminClicked(): void {
+    this.isAdmin = !this.isAdmin;  // Toggle between admin view and default view
+    this.router.navigate(['/admin/user-management']);
   }
 }
